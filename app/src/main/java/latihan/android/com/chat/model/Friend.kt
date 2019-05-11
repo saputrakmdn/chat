2bc742c12b54d970539859419f4a97e95a1e0448
saputrakmdn <saputra.kmdn@gmail.com>
2019-05-11 15:34:06 +0700

    Initial commit

---
+++ app/src/main/java/latihan/android/com/chat/model/Friend.kt
@@ -0,0 +1,8 @@
+package  latihan.android.com.chat.model
+
+import  latihan.android.com.chat.data.ParseFirebaseData
+
+import java.io.Serializable
+
+
+class Friend(val id: String, val name: String, val photo: String) : Serializable
